0650af53480730eff7989fc40454de4bdfd8805e
Weslin Antonio Rodríguez Silva <weslin.rodriguez@est.ulsa.edu.ni>
2025-02-19 17:19:51 -0600

    Agregar modelo y migraciones para órdenes de proveedores, incluyendo relaciones con proveedores y usuarios.

---
+++ app/Models/SupplierOrder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SupplierOrder extends Model
+{
+    //
+    protected $fillable =[
+        "status", 
+        "order_date",
+        "delivery_date",
+    ];
+    
+    public function supplier(){
+        return $this->belongsTo(Supplier::class,
+        "supplier_id", "id");
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class,"user_id", "id");
+    }
+    protected function casts(): array{
+        return [
+            "order_date" => "date",
+            "delivery_date" => "date",
+        ];
+    }
+}
